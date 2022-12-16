175f1b9caf36293b0c8dfcdfc7fc4a3ca4f05dc5
ArtemiyKew <hezam228gg@gmail.com>
2022-12-16 09:29:37 +0400

    feat: add about us

--- src/components/aboutUs/AboutUs.tsx
+++ src/components/aboutUs/AboutUs.tsx
@@ -10,11 +10,35 @@ export default class AboutUs extends Component {
       <div className="body">
         <div className="about-section">
           <h1>About Us Page</h1>
-          <p>Some text about who we are and what we do.</p>
           <p>
-            Resize the browser window to see that this page is responsive by the
-            way.
+            Идея: Услуга, оказывающая помощь в поиске шаурмы, неважно, где ты
+            находишься, а важно, что при помощи нашего стартап-проекта вы всегда
+            сможете вкусно покушать, выбрав лучший вариант, как в плане
+            качества, так и в плане экономии времени
           </p>
+          <h1>Анализ конкурентов</h1>
+          <ul className="competitor">
+            <li>
+              1. Где шаурма? Это наш главный и единственный конкурент. Создатель
+              приложения является Dodo engineering
+            </li>
+            <li>Плюсы данного конкурента:</li>
+            <li>1. Имеется мобильное приложение.</li>
+            <li>
+              2. Более расширенный поиск мест для питания, за счет использования
+              множества api геоданных.
+            </li>
+            <li>
+              3. В приложении есть оценивание мест питания, фотографии и тд.
+            </li>
+
+            <li>Минусы данного конкурента:</li>
+            <li>
+              1. Нет веб сайта, чтобы воспользоваться их услугами надо скачивать
+              приложение.
+            </li>
+            <li>2. Нет построения маршрута.</li>
+          </ul>
         </div>
 
         <h2>Our Team</h2>
@@ -29,7 +53,7 @@ export default class AboutUs extends Component {
               <div className="container">
                 <h2>Artemiy Savinov</h2>
                 <p className="title">CEO & Founder</p>
-                <p>Make yandex map and serach shaurma</p>
+                <p>Make yandex map</p>
                 <p>flartemiy@gmail.com</p>
                 <p>
                   <button
@@ -50,8 +74,8 @@ export default class AboutUs extends Component {
               <img src={require("./img/akim.jpg")} alt="Mike" className="img" />
               <div className="container">
                 <h2>Akim Yashin</h2>
-                <p className="title">Second Developer</p>
-                <p>Make navigation bar and this page "About us"</p>
+                <p className="title">Developer</p>
+                <p>Make "About us"</p>
                 <p>mike@example.com</p>
                 <p>
                   <button
@@ -59,6 +83,29 @@ export default class AboutUs extends Component {
                     onClick={() =>
                       this.openInNewTab("https://t.me/infoshopfish")
                     }
+                  >
+                    Contact me
+                  </button>
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="column">
+            <div className="card">
+              <img
+                src={require("./img/alex.jpeg")}
+                alt="Mike"
+                className="img"
+              />
+              <div className="container">
+                <h2>Alexsey Mokeev</h2>
+                <p className="title">Developer</p>
+                <p>Make navigation bar</p>
+                <p>mike@example.com</p>
+                <p>
+                  <button
+                    className="button"
+                    onClick={() => this.openInNewTab("https://t.me/iveekom")}
                   >
                     Contact me
                   </button>
